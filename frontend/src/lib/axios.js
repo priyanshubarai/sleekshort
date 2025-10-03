aa387271e99ce0314ccc78362af7ce0a353a817b
priyanshubarai <priyanshubarai45@gmail.com>
2025-10-03 19:27:10 +0530

    local link used

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 
-const baseURL = import.meta.env.MODE === "development"
-  ? import.meta.env.VITE_DEV_SERVER_URL
-  : import.meta.env.VITE_SERVER_URL;
+// const baseURL = import.meta.env.MODE === "development"
+//   ? import.meta.env.VITE_DEV_SERVER_URL
+//   : import.meta.env.VITE_SERVER_URL;
 
 const apiInstance = axios.create({
-  baseURL,
+  baseURL: "http://localhost:7000",
   timeout: 1000,
   headers: {'X-Custom-Header': 'foobar'}
 });
